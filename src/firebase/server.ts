1430f25bc39ccac195d0e189a8b4ae96c3a6bd24
Kenney Curlee <bassetthnd824@gmail.com>
2025-08-09 19:50:11 -0500

    trying again

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -5,7 +5,9 @@ import { Auth, getAuth } from 'firebase-admin/auth'
 
 const SESSION_COOKIE = 'daily-quiz-session'
 const currentApps = getApps()
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!)
+const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT
+console.log('[my json string]', serviceAccountJson)
+const serviceAccount = JSON.parse(serviceAccountJson!)
 let firestore: Firestore | undefined = undefined
 let auth: Auth | undefined = undefined
 
